c5c9350b75e7913e81f9e986d8e67d3c8a6f5acd
Mike Kerslake <theonlymikeever@gmail.com>
2017-07-26 21:53:06 -0400

    ready for live

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 const pg = require('pg');
 
-const client = new pg.Client('postgres://localhost/twitterdb');
+const client = new pg.Client(process.env.DATABASE_URL);
 
 client.connect(function(err){
   if (err){
